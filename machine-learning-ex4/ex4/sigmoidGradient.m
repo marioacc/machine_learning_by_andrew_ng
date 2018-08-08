371debf5e428a86daec0b58bdbb113c84f26a5de
marioacc <mario.acc.15@gmail.com>
2018-08-08 13:08:32 -0400

    Created sigmoid gradient function

--- machine-learning-ex4/ex4/sigmoidGradient.m
+++ machine-learning-ex4/ex4/sigmoidGradient.m
@@ -11,7 +11,9 @@ g = zeros(size(z));
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the gradient of the sigmoid function evaluated at
 %               each value of z (z can be a matrix, vector or scalar).
+sigmoid_gradient = sigmoid(z).*(1 - sigmoid(z));
 
+g=sigmoid_gradient
 
 
 
